d22fe3b46e05905ab35e14e9daacba206e094b90
Brian Farrell <brianrobotics@gmail.com>
2018-10-03 15:18:40 -0500

    intial pipeline test

---
+++ server/opensourcelist.py
@@ -0,0 +1,33 @@
+import json
+
+class OpenSourceSoftware:
+
+    def __init__(self):
+        self.OpenSourceSoftware = []
+
+    def add_OpenSourceSoftware(self, name, short_description, site):
+        new_oss = {}
+        new_oss["name"] = name
+        new_oss["short_desciption"] = short_description
+        new_oss["site"] = site
+        self.OpenSourceSoftware.append(new_oss)
+
+    def delete_OpenSourceSoftware(self, name):
+        found = False
+        for i, oss_entry in enumerate(self.OpenSourceSoftware):
+            if oss_entry["name"] == name:
+                index = i
+                found = True
+                del self.OpenSourceSoftware[i]
+        print("name: {0}".format(json.dumps(self.OpenSourceSoftware)))
+
+    def list(self):
+        return self.OpenSourceSoftware
+
+    def json_list(self):
+        return json.dumps(self.OpenSourceSoftware)
+
+oss = OpenSourceSoftware()
+oss.add_OpenSourceSoftware(name = "OpenSSL", short_description="OpenSSL Package", site = "https://www.openssl.org")
+oss.add_OpenSourceSoftware(short_description="SSH Package", site = "https://www.openssh.org", name="openssh")
+print (oss.json_list())
